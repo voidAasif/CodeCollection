9852b43d5db7abed02b3bd9f266768d1d6cf1699
voidAasif <aasifsaifi9280@gmail.com>
2024-09-12 12:27:44 +0530

    add some comment

--- javaHub/FrameSwing/code/j038_winListeners.java
+++ javaHub/FrameSwing/code/j038_winListeners.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
-class MainFrame extends JFrame implements WindowListener{
+class MainFrame extends JFrame implements WindowListener{ //contains 7 methods;
 
 
     MainFrame(){
@@ -15,23 +15,23 @@ class MainFrame extends JFrame implements WindowListener{
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
 
-        this.addWindowListener(this);
+        this.addWindowListener(this); //add listener on this frame;
 
         this.setVisible(true);
     }
 
     @Override
-    public void windowActivated(WindowEvent arg0) {
+    public void windowActivated(WindowEvent arg0) { //when window is on focus; (! Deactivated);
         System.out.println("Window Activated");
     }
 
     @Override
-    public void windowClosed(WindowEvent arg0) {
+    public void windowClosed(WindowEvent arg0) { //when window closed; (! Opened);
         System.out.println("Window Closed");
     }
 
     @Override
-    public void windowClosing(WindowEvent arg0) {
+    public void windowClosing(WindowEvent arg0) { //when window try to closing;
         System.out.println("Window Closing");
         // JOptionPane.showMessageDialog(null, "Closing after 5 seconds");
         // try {
@@ -42,22 +42,22 @@ class MainFrame extends JFrame implements WindowListener{
     }
 
     @Override
-    public void windowDeactivated(WindowEvent arg0) {
+    public void windowDeactivated(WindowEvent arg0) { //when window not on focus;
         System.out.println("Window Deactivated");
     }
 
     @Override
-    public void windowDeiconified(WindowEvent arg0) {
+    public void windowDeiconified(WindowEvent arg0) { // revert to current state(maximize); (! Iconified);
         System.out.println("Windows Deiconified");
     }
 
     @Override
-    public void windowIconified(WindowEvent arg0) {
+    public void windowIconified(WindowEvent arg0) { // when window minimize;
         System.out.println("Window IconiFied");
     }
 
     @Override
-    public void windowOpened(WindowEvent arg0) {
+    public void windowOpened(WindowEvent arg0) { //when window opened;
         System.out.println("WIndow Opened");
     }
 }
