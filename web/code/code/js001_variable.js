5055d0cdff9ef0e02eb7b916237906a78f00d68f
voidAasif <aasifsaifi9280@gmail.com>
2024-09-13 09:19:25 +0530

    perform some changes in js

--- web/code/code/js001_variable.js
+++ web/code/code/js001_variable.js
@@ -6,9 +6,10 @@ const c = 7;
 
 // var a = 1; 
 
-// let b = 1; // error (not declear twice);
+// let b = 1; // error (not declare twice);
 
-// c = 1; // error (overriting not possible);
+// c = 1; // error (overwriting not possible);
 
 
 document.write(a +" "+ b +" "+ c);
+console.log(a +" "+ b +" "+ c);
