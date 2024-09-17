14e5daebfce6dcdab31d015c3dd5193fad205461
voidAasif <aasifsaifi9280@gmail.com>
2024-09-17 14:39:04 +0530

    git commit -i "change instance in dbmanage file"

--- javaHub/project004/Goals/src/DBase/DBManagement.java
+++ javaHub/project004/Goals/src/DBase/DBManagement.java
@@ -3,10 +3,11 @@ package DBase;
 import goalInputCards.*;
 
 public class DBManagement  {
-    GoalName goalName = new GoalName();
+    GoalName goalName; 
     
     public DBManagement(){
-        goalName.setGoalName();
+        goalName = new GoalName();
+        // goalName.setGoalName();
         System.out.println("GoalName: "+goalName.getGoalName()); //not working;
     }
 }
