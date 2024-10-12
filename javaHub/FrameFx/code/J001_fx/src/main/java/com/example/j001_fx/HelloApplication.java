ae03ff779b4bbef2f26acff8bb036462e33fea1f
voidAasif <aasifsaifi9280@gmail.com>
2024-10-12 10:47:49 +0530

    basic fx

--- javaHub/FrameFx/code/J001_fx/src/main/java/com/example/j001_fx/HelloApplication.java
+++ javaHub/FrameFx/code/J001_fx/src/main/java/com/example/j001_fx/HelloApplication.java
@@ -1,8 +1,9 @@
 package com.example.j001_fx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +11,31 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+
+        //root node or layout manager;
+        TilePane root = new TilePane();
+        root.setStyle("-fx-background-color: linear-gradient(to right, red, blue);");
+
+
+        //Scene or panel; (Scene only work as a Container)
+        Scene panel = new Scene(root, 400, 250); //layout, width, height;
+
+
+        //Stage or frame;
+        stage.setScene(panel); //add scene into stage;
+        stage.setTitle("First Fx Frame");
+//        stage.setX(1300); //display frame on custom position;
+//        stage.setY(100);
+//        stage.setAlwaysOnTop(true);
+//        stage.setFullScreen(true);
+//        stage.setFullScreenExitHint("hello");
+//        stage.setIconified(true); //minimize;
+        stage.setMaximized(true);
+
+
+
+
+        stage.show(); //display stage;
     }
 
     public static void main(String[] args) {
